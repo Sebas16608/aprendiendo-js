9e92e94aa814bdcbcdbe947edee08439f3687ad8
Sebas16608 <asebasrr444@gmail.com>
2025-11-12 18:43:04 -0600

    Aprendiendo los datos de js

---
+++ datos.js
@@ -0,0 +1,15 @@
+let string = "Hola";
+console.log(typeof(string))
+console.log(string)
+
+let int = 1;
+console.log(typeof(int))
+console.log(int)
+
+let double = 2.5;
+console.log(typeof(double))
+console.log(double)
+
+let esMayor = true;
+console.log(typeof(esMayor))
+console.log(esMayor)
