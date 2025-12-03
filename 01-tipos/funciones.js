a7a95d76408d2686f52a9f801bb99b82484e78c9
Sebas16608 <asebasrr444@gmail.com>
2025-12-03 16:14:24 -0600

    Aprendiendo funciones2

---
+++ 01-tipos/funciones.js
@@ -0,0 +1,15 @@
+function saludar(){
+    console.log("Hola Mundo");
+}
+
+saludar();
+
+function suma(a, b){
+    return a + b;
+}
+
+let num1 = 1;
+let num2 = 3;
+
+let resultado = suma(num1, num2);
+console.log(resultado);
