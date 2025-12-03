338dd74f02fdbdc7cf2a9b4b2c84e8807e203d30
Sebas16608 <asebasrr444@gmail.com>
2025-12-03 16:10:34 -0600

    Aprendiendo arrays (listas en python)

---
+++ 01-tipos/arrays.js
@@ -0,0 +1,15 @@
+let animal = ["chanchito", "perro"];
+
+console.log(animal);
+console.log(animal[0]);
+animal[2] = "gato";
+console.log(animal);
+
+animal[10] = "dragon";
+console.log(animal[7]);
+
+console.log(typeof animal);
+
+// Objetos
+
+console.log(animal.length)
