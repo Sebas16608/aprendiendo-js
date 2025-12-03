0fc65a845eb0f59bd55ec49572dbf0c20f777aed
Sebas16608 <asebasrr444@gmail.com>
2025-12-03 16:48:17 -0600

    Aprendiendo operadores logicos

---
+++ 02-operadores/logicos.js
@@ -0,0 +1,17 @@
+// AND, OR, NOT
+
+// AND
+
+// console.log(true && true);
+// console.log(false && true);
+
+let mayor = true;
+let suscrito = true;
+console.log("operador and", mayor && suscrito)
+
+// OR
+console.log("operador or", mayor || suscrito);
+
+// NOT !
+console.log("operador not", !mayor);
+let soloCatalogoInfantil = !mayor;
